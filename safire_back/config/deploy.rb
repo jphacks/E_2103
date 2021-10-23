ed2f494045b6e057c8afd1cd16d38be3bd18bc2c
yuta.kawakami <kami.981108@gmail.com>
2021-10-23 18:21:16 +0900

    Fixed some settings

--- safire_back/config/deploy.rb
+++ safire_back/config/deploy.rb
@@ -25,7 +25,6 @@ set :deploy_to, "/var/www/#{fetch :application}/safire_back"
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 append :linked_files, "config/master.key"
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads", "public/assets"
