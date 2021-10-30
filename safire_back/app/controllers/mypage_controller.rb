882555d8633f5290fbc283c05fe72bb8edfb8fc3
kami9811 <49345024+kami9811@users.noreply.github.com>
2021-10-31 04:46:56 +0900

    Feature/mypage info (#37)
    * Dev/back 0 (#27)
    
    * Fixed
    
    * Fixed (#14)
    
    * Feature/test (#15)
    
    * Fixed
    
    * Actions test
    
    * Actions test
    
    * Fixed Actions (#16)
    
    * Added practice information function (#24)
    
    * Fixed for practice result
    
    * Fixed about practice times management
    
    * Fixed about practice times management
    
    * Dev/front 0 (#30)
    
    * Fixed
    
    * Fixed (#14)
    
    * Feature/test (#15)
    
    * Fixed
    
    * Actions test
    
    * Actions test
    
    * Fixed Actions (#16)
    
    * Fixed permission process (#23)
    
    * Added practice information function (#24)
    
    * Feature/p dash (#28)
    
    * Fixed
    
    * Fixed (#14)
    
    * Feature/test (#15)
    
    * Fixed
    
    * Actions test
    
    * Actions test
    
    * Added practice dashboard
    
    * Fixed permission process (#23)
    
    * Added practice dashboard
    
    * Added handling function for target information
    
    * Fixed dashboard
    
    * Added practice progress processing
    
    * Fixed permission process (#23)
    
    * Feature/p dash (#28)
    
    * Fixed
    
    * Fixed (#14)
    
    * Feature/test (#15)
    
    * Fixed
    
    * Actions test
    
    * Actions test
    
    * Added practice dashboard
    
    * Fixed permission process (#23)
    
    * Added practice dashboard
    
    * Added handling function for target information
    
    * Fixed dashboard
    
    * Added practice progress processing
    
    * Added beautiful practice UX/UI (#33)
    
    * readme update #1
    
    * Update README.md
    
    * Update README.md
    
    * Fixed to mypage ui information
    
    Co-authored-by: hsgwKei <63716217+hsgwKei@users.noreply.github.com>

--- safire_back/app/controllers/mypage_controller.rb
+++ safire_back/app/controllers/mypage_controller.rb
@@ -5,7 +5,10 @@ class MypageController < ApplicationController
 
     # Project list
     projects = Project.where(user_id: mypage_params["id"]).order(created_at: "DESC")
+    total_post = Project.where(user_id: mypage_params["id"]).count
     projects_list = []
+    total_practice = 0
+    total_clear = 0
     projects.each do |project|
       # タグを探して
       tags = ProjectTag.where(project_id: project.id)
@@ -16,6 +19,12 @@ class MypageController < ApplicationController
       one_object = one_object.select {|k,v| object_list.include?(k)}
       # push
       projects_list.push(one_object)
+      # クリアチェック
+      total_clear += 1 if !project.smile_result.nil? && (project.smile_result >= project.smile_times) && \
+                          !project.filler_result.nil? && (project.filler_result < project.filler_times) && \
+                          !project.negative_result.nil? && (project.negative_result < project.negative_times) && \
+                          !project.time_result.nil? && (project.time_result <= 30)
+      total_practice += project.project_times
     end
 
     # Liked Project list
@@ -34,7 +43,10 @@ class MypageController < ApplicationController
       liked_projects_list.push(one_object)
     end
 
-    render :json => { message: "マイページの情報の取得に成功しました.", user_id: mypage_params["id"], description: profile.description, own_project_list: projects_list, liked_project_list: liked_projects_list }
+    render :json => { message: "マイページの情報の取得に成功しました.", user_id: mypage_params["id"],
+    　　　　　　　　　　　description: profile.description, own_project_list: projects_list,
+    　　　　　　　　　　　liked_project_list: liked_projects_list,
+                      total_post: total_post, total_clear: total_clear, total_practice: total_practice }
   end
 
   private
