e88b7d577497c6c896b82e3a1f5daaec25cecad9
kami9811 <49345024+kami9811@users.noreply.github.com>
2022-02-19 17:22:52 +0900

    Feature/change nlp domain (#102)
    * Fixed README
    
    * Fixed feedback-page for laptop-usage (#44)
    
    * Fixed camera control
    
    * Fixed auto-presentation-generate system for high-through-put (#46)
    
    * Fixed README
    
    * Fixed article ranking (#50)
    
    * Fixed article ranking
    
    * Fixed home view
    
    * Fixed README
    
    * Fixed practice UX
    
    * Fixed sequence error
    
    * Fixed README
    
    * Fixed README
    
    * Fixed sequence error
    
    * Fixed rasa api
    
    * Fixed README
    
    * Fixed README
    
    * Fixed rasa api
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Ruby in home & edit page (#60)
    
    * Feature/ruby (#63)
    
    * Ruby in home & edit page
    
    * Completed ruby change
    
    * Completed ruby change
    
    * first commit at feature/home_refact
    
    * feature/home_refact #02
    
    * feature/home_refact(#03)
    
    * feature/home_refact (#04)
    
    * Feature/tutorial (#78)
    
    * Added tutorial popover to home
    
    * Added tutorial and header component
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed README
    
    * Fixed article ranking (#50)
    
    * Fixed article ranking
    
    * Fixed home view
    
    * Fixed README
    
    * Fixed practice UX
    
    * first commit at feature/home_refact
    
    * feature/home_refact #02
    
    * feature/home_refact(#03)
    
    * feature/home_refact (#04)
    
    * Feature/tutorial (#78)
    
    * Added tutorial popover to home
    
    * Added tutorial and header component
    
    * feature/errorAlert (#0)
    
    * feature/errorAlert (#1)
    
    * feature/errorAlert_2 (#1)
    
    * feature/errorAlert_2 (#2)
    
    * feature/errorAlert_2 (#3)
    
    * Feature/pip timer (#87)
    
    * Added data management system for practice-tracking (#85)
    
    * Added pip-timer for practice
    
    * Added pip-timer for practice
    
    * Fixed some bad points
    
    * Changed filler repository
    
    Co-authored-by: hsgwKei <kei1107.1455@gmail.com>
    Co-authored-by: hsgwKei <63716217+hsgwKei@users.noreply.github.com>

--- safire_front/src/environments/environment.prod.ts
+++ safire_front/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: "https://techfusion-studio.com/safire/",
-  filler: "https://68ae-114-184-50-180.ngrok.io/"
+  filler: "https://safire-betterhack.herokuapp.com/"
 };
